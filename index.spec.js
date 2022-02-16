653daa0dbb00dac7b77276d5b80eb98352fbd730
marcos <marcosssss1991@gmail.com>
2022-02-16 13:41:49 -0300

    test

--- index.spec.js
+++ index.spec.js
@@ -16,7 +16,7 @@ describe('Test my app server', () => {
         const res = await request(app).get('/teste1')
 
         expect(res.statusCode).toEqual(200)
-        expect(res.body).toHaveProperty('test1')
+        expect(res.body).toHaveProperty('message')
     })
 })
 
